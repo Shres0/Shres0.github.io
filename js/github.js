a0b08e076235a49031b81ee61ace285f693e3cf8
Shreyash Patil <102778395+Shres0@users.noreply.github.com>
2026-02-13 22:03:59 +0530

    Update github.js

--- js/github.js
+++ js/github.js
@@ -2,9 +2,7 @@ fetch("https://api.github.com/users/Shres0/repos")
 .then(res=>res.json())
 .then(data=>{
   const container=document.getElementById("githubProjects");
-  data.slice(0,6).forEach(repo=>{
-    const div=document.createElement("div");
-    div.innerHTML=`<p><strong>${repo.name}</strong><br>${repo.description||""}</p>`;
-    container.appendChild(div);
+  data.slice(0,5).forEach(repo=>{
+    container.innerHTML+=`<p><strong>${repo.name}</strong> - ${repo.description||""}</p>`;
   });
 });
